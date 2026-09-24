2b754656c0fe79f702eb6b9b1f8364e1b32e4745
Ikeoxo14 <ikeokoroafor8@gmail.com>
2026-09-23 18:08:27 -0700

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -8,3 +8,14 @@ JOIN player_stats AS s
     ON p.player_id = s.player_id
 WHERE s.season = 2025
 ORDER BY scrimmage_yards DESC;
+-- Compare total scrimmage yards and touchdowns by position
+SELECT
+    p.position,
+    SUM(s.rushing_yards + s.receiving_yards) AS total_scrimmage_yards,
+    SUM(s.total_tds) AS total_touchdowns
+FROM players AS p
+JOIN player_stats AS s
+    ON p.player_id = s.player_id
+WHERE s.season = 2025
+GROUP BY p.position
+ORDER BY total_scrimmage_yards DESC;
